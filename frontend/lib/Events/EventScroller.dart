42d0dfe69ba7f28cbcdeb5046271ef493f2476b0
rickisXD <rickyyu030102@gmail.com>
2022-04-28 20:35:30 -0700

    added content to event card

--- frontend/lib/Events/EventScroller.dart
+++ frontend/lib/Events/EventScroller.dart
@@ -10,24 +10,25 @@ class EventScroller extends StatefulWidget {
 
 class _EventScrollerState extends State<EventScroller> {
   List<String> eventNames = [
-    "Event 1",
-    "Event 2",
-    "Event 3",
-    "Event 4",
-    "Event 5"
+    "Shakespeare at The Park",
+    "Robespierre at The Park",
+    "Thoreau at The Park",
+    "Emerson at The Park",
+    "Erdös at The Park"
   ];
   @override
   Widget build(BuildContext context) {
     return Container(
       margin: const EdgeInsets.symmetric(vertical: 20.0),
-      height: 200.0,
+      height: 240.0,
       child: ListView.builder(
         // This next line does the trick.
 
         scrollDirection: Axis.horizontal,
         itemCount: eventNames.length,
         itemBuilder: (context, index) {
-          return EventCard(eventName: eventNames[index]);
+          return EventCard(
+              eventName: eventNames[index], eventDate: DateTime.now());
         },
       ),
     );
