a5ae0c9d67e9678ab7b6cf778378b91b5e3b7206
Brandon Huynh <brandon.huynh@live.com>
2022-05-05 18:06:38 -0700

    Displayed Initial User on MapboxView.dart

--- frontend/lib/MapboxView.dart
+++ frontend/lib/MapboxView.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_map/flutter_map.dart';
+import 'package:user_location/user_location.dart';
 import 'package:flutter/material.dart';
 import 'package:frontend/Database.dart';
 import 'package:latlong2/latlong.dart' as latlng;
@@ -12,13 +13,35 @@ class MapboxView extends StatefulWidget {
 }
 
 class _MapboxViewState extends State<MapboxView> {
+  MapController mapController = MapController();
+  late UserLocationOptions userLocationOptions;
+  List<Marker> markers = [
+    Marker(
+      point: latlng.LatLng(35.2828, -120.6596),
+      builder: (BuildContext context) { return Container(
+        decoration: BoxDecoration(color: Colors.blue, shape: BoxShape.circle),
+        width: 12,
+        height: 12,
+        padding: const EdgeInsets.all(10),
+      ); },
+      width: 30.0,
+      height: 30.0,)];
   @override
   Widget build(BuildContext context) {
     MongoDatabase.getDocuments();
+
+    userLocationOptions = UserLocationOptions(
+      context: context,
+      mapController: mapController,
+      markers: markers,
+    );
     return FlutterMap(
         options: MapOptions(
           center: latlng.LatLng(35.2828, -120.6596),
           zoom: 13.0,
+          plugins: [
+            UserLocationPlugin(),
+          ],
         ),
         layers: [
           TileLayerOptions(
@@ -28,7 +51,10 @@ class _MapboxViewState extends State<MapboxView> {
                 'id': 'mapbox.mapbox-streets-v8'
               }
           ),
-        ]
+          MarkerLayerOptions(markers: markers),
+          userLocationOptions,
+        ],
+        mapController: mapController,
     );
   }
 }
